eb1815b7aca35201becf42be696ecc5dbfc9cf93
jamlong <jamlong@abctech-thailand.com>
2014-02-12 17:11:54 +0700

    BT-2617:Create component to import data from table articlestat in php version into articles table : Fixed character problem

--- src/main/java/no/api/pulsimport/app/mapper/ArticleStatMapper.java
+++ src/main/java/no/api/pulsimport/app/mapper/ArticleStatMapper.java
@@ -1,10 +1,12 @@
 package no.api.pulsimport.app.mapper;
 
 import no.api.pulsimport.app.DateTimeFormatUtil;
+import no.api.pulsimport.app.StringCleaningUtil;
 import no.api.pulsimport.app.bean.ArticleStatResultSet;
 import no.api.pulsimport.app.bean.ArticleStatRow;
 import no.api.pulsimport.app.model.ArticleStatModel;
 import no.api.pulsimport.app.model.SiteModel;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -28,13 +30,15 @@ public class ArticleStatMapper {
             articleStatModel.setVisit(Integer.parseInt(eachRow.getField().get(3)));
             articleStatModel.setDate(DateTimeFormatUtil.parseDateTime(eachRow.getField().get(4)));
             articleStatModel.setArticleId(eachRow.getField().get(5));
-            articleStatModel.setArticleTitle(eachRow.getField().get(6));
-            articleStatModel.setArticleUrl(eachRow.getField().get(7));
+            articleStatModel.setArticleTitle( StringCleaningUtil.unescapeHtmlAndCapitalizeAndReplaceDash(eachRow.getField().get(6)));
+            articleStatModel.setArticleUrl(StringEscapeUtils.unescapeHtml(eachRow.getField().get(7)));
             articleStatModel.setSite(siteModel);
+
 
             articleStatModels.add(articleStatModel);
         }
 
         return articleStatModels;
     }
+
 }
